8b2c23bc10218f2516f29d8a95d6f27378edd059
BethM <medina.beth@gmail.com>
2012-04-10 13:30:25 -0600

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -87,44 +87,6 @@ describe "Authentication" do
         end
       end
 
-      describe "in the Users controller" do
-
-        describe "visiting the edit page" do
-          before { visit edit_user_path(user) }
-          it { should have_selector('title', text: 'Sign in') }
-        end
-
-        describe "submitting to the update action" do
-          before { put user_path(user) }
-          specify { response.should redirect_to(signin_path) }
-        end
-
-        describe "visiting the user index" do
-          before { visit users_path }
-          it { should have_selector('title', text: 'Sign in') }
-        end
-      end
-    end
-
-    describe "as wrong user" do
-      let(:user) { FactoryGirl.create(:user) }
-      let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
-      before { sign_in user }
-
-      describe "visiting Users#edit page" do
-        before { visit edit_user_path(wrong_user) }
-        it { should have_selector('title', text: full_title('')) }
-      end
-
-      describe "submitting a PUT request to the Users#update action" do
-        before { put user_path(wrong_user) }
-        specify { response.should redirect_to(root_path) }
-      end
-    end
-
-    describe "for non-signed-in users" do
-      let(:user) { FactoryGirl.create(:user) }
-
       describe "when attempting to visit a protected page" do
         before do
           visit edit_user_path(user)
@@ -148,6 +110,62 @@ describe "Authentication" do
             end
           end
         end
+      end
+
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { response.should redirect_to(signin_path) }          
+        end
+      end
+
+      describe "in the Users controller" do
+
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "submitting to the update action" do
+          before { put user_path(user) }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+      end
+    end
+
+    describe "as wrong user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+      before { sign_in user }
+
+      describe "visiting Users#edit page" do
+        before { visit edit_user_path(wrong_user) }
+        it { should have_selector('title', text: full_title('')) }
+      end
+
+      describe "submitting a PUT request to the Users#update action" do
+        before { put user_path(wrong_user) }
+        specify { response.should redirect_to(root_path) }
       end
     end
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "Static pages" do
+describe "StaticPages" do
 
   subject { page }
 
@@ -20,8 +20,8 @@ describe "Static pages" do
     describe "for signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
       before do
-        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
-        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        FactoryGirl.create(:micropost, :user => user, :content => "Lorem")
+        FactoryGirl.create(:micropost, :user => user, :content => "Ipsum")
         sign_in user
         visit root_path
       end
@@ -29,6 +29,65 @@ describe "Static pages" do
       it "should render the user's feed" do
         user.feed.each do |item|
           page.should have_selector("li##{item.id}", text: item.content)
+        end
+      end
+
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 follower",  href: followers_user_path(user)) }
+      end
+    end
+
+    describe "pagination" do
+
+      let(:user) { FactoryGirl.create(:user) }
+
+      before do
+        sign_in user
+        50.times { FactoryGirl.create(:micropost, user: user, content: "stuff") }
+        visit root_path
+      end
+
+      after(:all)  { Micropost.delete_all }
+
+      let(:first_page)  { Micropost.paginate(page: 1) }
+      let(:second_page) { Micropost.paginate(page: 2) }
+
+      it { should have_link('Next') }
+      it { should have_link('2') }
+      it { should have_link('delete') }
+
+      it "should list each micropost" do
+        Micropost.all[0..2].each do |micropost|
+          page.should have_selector('li', text: micropost.content)
+        end
+      end
+
+      it "should list the first page of microposts" do
+        first_page.each do |micropost|
+          page.should have_selector('li', text: micropost.content)
+        end
+      end
+
+      #it "should not list the second page of microposts" do
+      #  second_page.each do |micropost|
+      #    page.should_not have_selector('li', text: micropost.content)
+      #  end
+      #end
+
+      describe "showing the second page" do
+        before { visit root_path(page: 2) }
+
+        it "should list the second page of users" do
+          second_page.each do |micropost|
+            page.should have_selector('li', text: micropost.content)
+          end
         end
       end
     end
